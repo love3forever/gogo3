dfab62a8c6ec1b11ba9ad3ad7f345516d9c050f0
honey3go <honey3_go@163.com>
2017-05-17 22:33:06 +0800

    topbar_style

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <hello></hello>
+    <topbar></topbar>
   </div>
 </template>
 
 <script>
-import Hello from './components/Hello'
+import topbar from './components/topbar'
 
 export default {
   name: 'app',
   components: {
-    Hello
+    topbar
   }
 }
 </script>
@@ -23,6 +22,5 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
---
+++ src/components/topbar.vue
@@ -0,0 +1,82 @@
+<template>
+  <div class="topbar">
+    <h1 class="inbl"><a href="/#">网易云音乐</a></h1>
+    <ul class="inbl">
+      <li class="inbl" v-for="item in title"><a href="/#">{{item}}</a></li>
+    </ul>
+    <div class="wrap">
+      <input name="search" placeholder="单曲/歌手/专辑/歌单/MV/用户">
+      <div class="wrap">
+        
+      </div>
+    </div>
+    <a href="/#"></a>
+    <div class="wrap" id="logwrap" @mouseover="showLogmethods" @mouseout="hideLogmethods">
+      <a href="">登录</a>
+      <div class="wrap" id="logmethods" style="display:none">
+        <ul>
+          <li v-for="item in log"><a href="/#">{{item}}</a></li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'topbar',
+  data () {
+    return {
+      title: ['发现音乐','我的音乐','朋友','商城','音乐人','下载客户端'],
+      log:['手机号登录','微信登录','QQ登录','新浪微博登录','网易邮箱账号登录']
+    }
+  },
+  methods:{
+    showLogmethods:function(){
+      document.getElementById('logmethods').style.display='';
+    },
+    hideLogmethods:function(){
+      document.getElementById('logmethods').style.display='none';
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.inbl {
+  display: inline-block;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  padding: 0 10px;
+  text-align: left;
+}
+
+a {
+  text-decoration:none
+}
+
+input {
+  width: 210px;
+  border-radius: 10px;
+}
+
+.wrap{
+  display: inline-block;  
+}
+#logwrap{
+  position: relative;
+}
+#logmethods{
+  width: 150px;
+  position: absolute;
+  top: 10px;
+  left: -50px;
+}
+</style>
