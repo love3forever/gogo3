62775421cb2a0bd25ee152685334b72775319751
love3forever <wm_gaming@163.com>
2017-07-17 17:04:22 +0800

    remote deploy try

--- src/backend/fabfile.py
+++ src/backend/fabfile.py
@@ -8,6 +8,9 @@
 from fabric.api import *
 from fabric.contrib.console import confirm
 
+env.use_ssh_config = True
+env.hosts = ['al']
+
 
 def run_test():
     with settings(warn_only=True):
@@ -28,3 +31,21 @@ def pre_deployed():
     run_test()
     commit()
     push()
+
+
+def deploy():
+    code_dir = '~/git/gogo3'
+    run('ls')
+    with settings(warn_only=True):
+        if run("ls {}".format(code_dir)).failed:
+            run("git clone https://github.com/love3forever/gogo3.git {}".format(code_dir))
+    with cd(code_dir):
+        run("git pull")
+        run("sudo pip install -r ./src/backend/requirements.txt")
+        run("nohup gunicorn -w 2 -b 0.0.0.0:33333 ./src/backend/api/server:app &")
+    # with cd(code_dir):
+    #     run("ps -ef | grep 0.0.0.0:33333 | grep -v grep \
+    #         | awk '{print $2}' | head -n 1 | xargs kill")
+    #     print('old process killed')
+    #     run('git pull origin master')
+    #     run('nohup gunicorn -w 2 -b 0.0.0.0:33333 server:app &')
